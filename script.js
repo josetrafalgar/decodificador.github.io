b7a9c07f13b9aaadcfc067a778166e31f9f92d09
José Trafalgar <50798663+josetrafalgar@users.noreply.github.com>
2022-05-04 07:28:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,14 +10,15 @@ function criptografar() {
         inputSaida.value = "Por Favor Digite Alguma Coisa";
     }
     else {
-        var text = inputEntrada.value;
-        var result = text.replace(/e/g, "enter");
-        var result1 = text.replace(/i/g, "imes");
-        var result2 = text.replace(/a/g, "ai");
-        var result3 = text.replace(/u/g, "ufat");
-        var result4 = text.replace(/o/g, "ober");
+        var text = inputEntrada.value; //pegando do input do usuario
+        var result  =  text.replace(/e/g, 'enter'); // irá converter tudo de "e" para "enter"
+        var result2 = result.replace(/i/g, 'imes'); // irá converter tudo de "i" para "imes" 
+        var result3 = result2.replace(/a/g, 'ai');
+        var result4 = result3.replace(/o/g, 'ober');
+        var result5 = result4.replace(/u/g, 'ufat');
 
-        inputSaida.value = result4;
+        inputSaida.value= result5;
+
     }
     
 }
@@ -29,10 +30,10 @@ function descriptografar() {
     else {
         var text = inputEntrada.value;
         var result = text.replace(/enter/g, "e");
-        var result1 = text.replace(/imes/g, "i");
-        var result2 = text.replace(/ai/g, "a");
-        var result3 = text.replace(/ufat/g, "u");
-        var result4 = text.replace(/ober/g, "o");
+        var result1 = result.replace(/imes/g, "i");
+        var result2 = result1.replace(/ai/g, "a");
+        var result3 = result2.replace(/ufat/g, "u");
+        var result4 = result3.replace(/ober/g, "o");
 
         inputSaida.value = result4;
     }
